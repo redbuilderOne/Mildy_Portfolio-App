e073774c1908ef46c64e9de29d882752e5084e6f
redbuilderOne <forsaken2492@yandex.ru>
2022-07-04 17:23:38 +0300

    first commit

---
+++ Mildy/Mildy/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Mildy
+//
+//  Created by Дмитрий Скворцов on 04.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemCyan
+        // Do any additional setup after loading the view.
+    }
+
+    
+
+}
+
